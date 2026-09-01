0c913ad36726c158b51f015c40ed6bd7312bc35d
qualidadeacosvital04-sketch <qualidadeacosvital04@gmail.com>
2026-09-01 19:16:02 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'rnc-av-v1';
+const ARQUIVOS = [
+  '/',
+  '/index.html',
+  '/RNC_Relatorio_Nao_Conformidade_01.html',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ARQUIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
